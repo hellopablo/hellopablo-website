637605e2e0a76824893336391a0397cd279755ad
Pablo de la Peña <p@shedcollective.org>
2015-12-15 09:51:25 +0000

    Brining up to date
    + Updated Nails
    + Updated Rollbar
    + Updated Shed Logo
    + Using Gulp for compiling assets

--- config/app.php
+++ config/app.php
@@ -4,4 +4,4 @@ define('APP_NAME', 'HelloPablo');
 define('APP_DEFAULT_TIMEZONE', 'Europe/London');
 define('APP_PRIVATE_KEY', 'ad602e782debc6364c6ebc2b3e52c720');
 define('APP_DEVELOPER_EMAIL', 'pablo@hellopablo.co.uk');
-define('APP_CDN_DRIVER', 'AWS_LOCAL');
+define('APP_CDN_DRIVER', 'nailsapp/driver-cdn-awslocal');
